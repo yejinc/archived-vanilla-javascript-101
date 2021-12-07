000d4d39c530f421f0a2c3e7542022f5523ee428
Jinnie Cho <yejincho1105@gmail.com>
2021-12-07 22:54:09 +0900

    🔨 Learned how to use await

--- async/async.js
+++ async/async.js
@@ -12,3 +12,42 @@ const user = fetchUser();
 user.then(console.log);
 console.log(user); 
 
+// 2. await ✨
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function orderTea() {
+  await delay(2000);
+  return '🍵';  
+}
+
+async function orderCoffee() {
+  await delay(1000);
+  return '☕'; 
+
+}
+
+async function pickupBeverages() {
+  const teaPromise = orderTea();
+  const coffeePromise = orderCoffee();
+  const tea = await teaPromise;
+  const coffee = await coffeePromise;
+  return `${tea} + ${coffee}`;
+}
+
+pickupBeverages().then(console.log);
+
+// 3. useful APIs ✨
+function pickupAllBeverages() {
+  return Promise.all([orderTea(), orderCoffee()]).then(beverages =>
+    beverages.join(' + ')
+  );
+}
+pickupAllBeverages().then(console.log);
+
+function pickupOnlyOne() {
+  return Promise.race([orderTea(), orderCoffee()]);
+}
+
+pickupOnlyOne().then(console.log);
